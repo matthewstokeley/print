4deb73e0491b1e4ce6c683725e4d7da2fdf19b6b
Matthew Stokeley <Matthew Stokeley>
2017-07-28 13:02:55 -0700

    wrapped List in an iffe module

--- src/list.js
+++ src/list.js
@@ -1,201 +1,206 @@
+(function(semantic) {
 
-/**
- * [semantic.List description]
- * @param {[type]} options [description]
- */
-var semantic.List = function semantic.List(options) {
-	this.returnString = options.returnString || true;
-	this.rootId = options.rootId;
-	this.role = options.role;
-	this.classList = options.classList;
-	this.data = options.data || {};
-	this.order = options.order || 'ul';
-	this.init(options);
-};
-
-/**
- * [init description]
- * @param  {Object} options 
- * @return {Object} returns the class context
- */
-semantic.List.prototype.init = function(options) {
-	return this.setItems(options.items)
-		.setLinks(options.items.map(function(item) { return item.link; }))
-		.setItemNodes()
-		.setLinkNodes()
-		.setListNode()
-		.setDocument()
-		.setString();
-};
-
-/**
- * [setItems description]
- * @chainable
- * @param {Array} items    array of list items
- */
-semantic.List.prototype.setItems = function(items) {
-	this.items = items.map(this.itemFactory.bind(this));
-	return this;
-};
-
-/**
- * [setLinks description]
- * @chainable
- * @param {Array} links     array of list links
- */
-semantic.List.prototype.setLinks = function(links) {
-	this.links = links.map(this.linkFactory.bind(this));
-	return this;
-};
-
-/**
- * [itemFactory description] 
- * @param  {Object} item
- * @return {Object}
- */
-semantic.List.prototype.itemFactory = function(item) {
-	return {
-		name: item.name,
-		id: (item.id) ? item.id : item.name,
-		classList: item.classList ? [this.rootId + '--link'].concat(item.classList) : [this.rootId + '--item'],
-		dataset: item.data ? item.data : {}
+	/**
+	 * [List description]
+	 * @param {[type]} options [description]
+	 */
+	var List = function List(options) {
+		this.returnString = options.returnString || true;
+		this.rootId = options.rootId;
+		this.role = options.role;
+		this.classList = options.classList;
+		this.data = options.data || {};
+		this.order = options.order || 'ul';
+		this.init(options);
 	};
-};
 
-/**
- * [linkFactory description]
- * @param  {Object} link
- * @return {Object}
- */
-semantic.List.prototype.linkFactory = function(link) {
-	return {
-		name: link.name,
-		text: link.value,
-		href: link.href ? link.href : 'javascript:;',
-		id: link.id ? link.id : link.name,
-		title: link.title ? link.title : link.name,
-		classList: link.classList ? [this.rootId + '--link'].concat(link.classList) : [this.rootId + '--link'],
-		dataset: link.data ? link.data : {}
-	}
-};
+	/**
+	 * [init description]
+	 * @param  {Object} options 
+	 * @return {Object} returns the class context
+	 */
+	List.prototype.init = function(options) {
+		return this.setItems(options.items)
+			.setLinks(options.items.map(function(item) { return item.link; }))
+			.setItemNodes()
+			.setLinkNodes()
+			.setListNode()
+			.setDocument()
+			.setString();
+	};
 
-/**
- * [setItemNodes description]
- * @chainable
- */
-semantic.List.prototype.setItemNodes = function() {
-	this.itemNodes = this.items.map(this.createItemNode.bind(this));
-	return this;
-};
+	/**
+	 * [setItems description]
+	 * @chainable
+	 * @param {Array} items    array of list items
+	 */
+	List.prototype.setItems = function(items) {
+		this.items = items.map(this.itemFactory.bind(this));
+		return this;
+	};
 
-/**
- * [setLinkNodes description]
- * @chainable
- */
-semantic.List.prototype.setLinkNodes = function() {
-	this.linkNodes = this.links.map(this.createLinkNode.bind(this));
-	return this;
-};
+	/**
+	 * [setLinks description]
+	 * @chainable
+	 * @param {Array} links     array of list links
+	 */
+	List.prototype.setLinks = function(links) {
+		this.links = links.map(this.linkFactory.bind(this));
+		return this;
+	};
 
-/**
- * [setListNode description]
- * @chainable
- */
-semantic.List.prototype.setListNode = function() {
-	this.listNode = $.createElement({
-		element: this.order,
-		properties: {
-			id: this.rootId,
-			classList: this.classList,
-			role: this.role
+	/**
+	 * [itemFactory description] 
+	 * @param  {Object} item
+	 * @return {Object}
+	 */
+	List.prototype.itemFactory = function(item) {
+		return {
+			name: item.name,
+			id: (item.id) ? item.id : item.name,
+			classList: item.classList ? [this.rootId + '--link'].concat(item.classList) : [this.rootId + '--item'],
+			dataset: item.data ? item.data : {}
+		};
+	};
+
+	/**
+	 * [linkFactory description]
+	 * @param  {Object} link
+	 * @return {Object}
+	 */
+	List.prototype.linkFactory = function(link) {
+		return {
+			name: link.name,
+			text: link.value,
+			href: link.href ? link.href : 'javascript:;',
+			id: link.id ? link.id : link.name,
+			title: link.title ? link.title : link.name,
+			classList: link.classList ? [this.rootId + '--link'].concat(link.classList) : [this.rootId + '--link'],
+			dataset: link.data ? link.data : {}
 		}
-	});
-	return this;
-};
+	};
 
-/**
- * [createItemNode description]
- * @chainable
- * @param  {Object} item
- * @return {Object}
- */
-semantic.List.prototype.createItemNode = function(item) {
-	return $.createElement({
-		element: 'li',
-		properties: item
-	});
-	return this;
-};
+	/**
+	 * [setItemNodes description]
+	 * @chainable
+	 */
+	List.prototype.setItemNodes = function() {
+		this.itemNodes = this.items.map(this.createItemNode.bind(this));
+		return this;
+	};
 
-/**
- * [createLinkNode description]
- * @param  {Object} link [description]
- * @return {Object}      [description]
- */
-semantic.List.prototype.createLinkNode = function(link) {
-	return $.createElement({
-		element: 'a',
-		properties: link
-	});
-};
+	/**
+	 * [setLinkNodes description]
+	 * @chainable
+	 */
+	List.prototype.setLinkNodes = function() {
+		this.linkNodes = this.links.map(this.createLinkNode.bind(this));
+		return this;
+	};
 
-/**
- * [createDocument description]
- * @return {Node}
- */
-semantic.List.prototype.createDocument = function() {
-	return $.createListFragment(this.listNode, this.itemNodes, this.linkNodes);
-};
+	/**
+	 * [setListNode description]
+	 * @chainable
+	 */
+	List.prototype.setListNode = function() {
+		this.listNode = $.createElement({
+			element: this.order,
+			properties: {
+				id: this.rootId,
+				classList: this.classList,
+				role: this.role
+			}
+		});
+		return this;
+	};
 
-/**
- * [setDocument description]
- * @chainable
- */
-semantic.List.prototype.setDocument = function() {
-	this.document = this.createDocument();
-	return this;
-};
+	/**
+	 * [createItemNode description]
+	 * @chainable
+	 * @param  {Object} item
+	 * @return {Object}
+	 */
+	List.prototype.createItemNode = function(item) {
+		return $.createElement({
+			element: 'li',
+			properties: item
+		});
+		return this;
+	};
 
-/**
- * [getDocument description]
- * @return {Node}
- */
-semantic.List.prototype.getDocument = function() {
-	return this.document;
-};
+	/**
+	 * [createLinkNode description]
+	 * @param  {Object} link [description]
+	 * @return {Object}      [description]
+	 */
+	List.prototype.createLinkNode = function(link) {
+		return $.createElement({
+			element: 'a',
+			properties: link
+		});
+	};
 
-/**
- * [createString description]
- * @return {String}
- */
-semantic.List.prototype.createString = function() {
-	return this.document.outerHTML;
-};
+	/**
+	 * [createDocument description]
+	 * @return {Node}
+	 */
+	List.prototype.createDocument = function() {
+		return $.createListFragment(this.listNode, this.itemNodes, this.linkNodes);
+	};
 
-/**
- * [setString description]
- * @chainable
- */
-semantic.List.prototype.setString = function() {
-	this.string = this.createString();
-	return this;
-};
+	/**
+	 * [setDocument description]
+	 * @chainable
+	 */
+	List.prototype.setDocument = function() {
+		this.document = this.createDocument();
+		return this;
+	};
 
-/**
- * [getString description]
- * @return {String} [description]
- */
-semantic.List.prototype.getString = function() {
-	return this.string;
-};
+	/**
+	 * [getDocument description]
+	 * @return {Node}
+	 */
+	List.prototype.getDocument = function() {
+		return this.document;
+	};
 
-/**
- * [appendToNode description]
- * @chainable
- * @param  {Node}
- * @return {Object}
- */
-semantic.List.prototype.appendToNode = function(node) {
-	node.appendChild(this.document);
-	return this;
-};
+	/**
+	 * [createString description]
+	 * @return {String}
+	 */
+	List.prototype.createString = function() {
+		return this.document.outerHTML;
+	};
+
+	/**
+	 * [setString description]
+	 * @chainable
+	 */
+	List.prototype.setString = function() {
+		this.string = this.createString();
+		return this;
+	};
+
+	/**
+	 * [getString description]
+	 * @return {String} [description]
+	 */
+	List.prototype.getString = function() {
+		return this.string;
+	};
+
+	/**
+	 * [appendToNode description]
+	 * @chainable
+	 * @param  {Node}
+	 * @return {Object}
+	 */
+	List.prototype.appendToNode = function(node) {
+		node.appendChild(this.document);
+		return this;
+	};
+
+	semantic.List = List;
+
+})(semantic);
